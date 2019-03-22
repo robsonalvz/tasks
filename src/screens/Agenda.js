8f344c4595ca37eae222a8ba3524e5e6ece8b85d
robsonalvz <robson.alves@dce.ufpb.br>
2019-03-22 10:19:30 -0300

    filtragem de tasks concluida

--- src/screens/Agenda.js
+++ src/screens/Agenda.js
@@ -1,5 +1,6 @@
-import React , {Component} from 'react'
-import {StyleSheet, Text, View, ImageBackground, FlatList,
+import React, { Component } from 'react'
+import {
+    StyleSheet, Text, View, ImageBackground, FlatList,
     TouchableOpacity,
     Platform,
 
@@ -11,88 +12,133 @@ import todayImage from '../../assets/imgs/today.jpg'
 import commonStyle from '../commonStyles'
 import Task from '../components/Task'
 import Icon from 'react-native-vector-icons/FontAwesome'
-export default class Agenda extends Component{
+export default class Agenda extends Component {
     state = {
-        tasks:[
-            {id:Math.random(), desc : "Comprar curso de react-native",
-                estimateAt:new Date(), doneAt:new Date()},
-            {id:Math.random(),desc : "Concluir o curso",
-                estimateAt:new Date(), doneAt:null},
-                {id:Math.random(), desc : "Comprar curso de react-native",
-                estimateAt:new Date(), doneAt:new Date()},
-            {id:Math.random(),desc : "Concluir o curso",
-                estimateAt:new Date(), doneAt:null},
-                {id:Math.random(), desc : "Comprar curso de react-native",
-                estimateAt:new Date(), doneAt:new Date()},
-            {id:Math.random(),desc : "Concluir o curso",
-                estimateAt:new Date(), doneAt:null},
-                {id:Math.random(), desc : "Comprar curso de react-native",
-                estimateAt:new Date(), doneAt:new Date()},
-            {id:Math.random(),desc : "Concluir o curso",
-                estimateAt:new Date(), doneAt:null},
-                {id:Math.random(), desc : "Comprar curso de react-native",
-                estimateAt:new Date(), doneAt:new Date()},
-            {id:Math.random(),desc : "Concluir o curso",
-                estimateAt:new Date(), doneAt:null},
-                {id:Math.random(), desc : "Comprar curso de react-native",
-                estimateAt:new Date(), doneAt:new Date()},
-            {id:Math.random(),desc : "Concluir o curso",
-                estimateAt:new Date(), doneAt:null},
-                {id:Math.random(), desc : "Comprar curso de react-native",
-                estimateAt:new Date(), doneAt:new Date()},
-            {id:Math.random(),desc : "Concluir o curso",
-                estimateAt:new Date(), doneAt:null},
-                {id:Math.random(), desc : "Comprar curso de react-native",
-                estimateAt:new Date(), doneAt:new Date()},
-            {id:Math.random(),desc : "Concluir o curso",
-                estimateAt:new Date(), doneAt:null},
+        tasks: [
+            {
+                id: Math.random(), desc: "Comprar curso de react-native",
+                estimateAt: new Date(), doneAt: new Date()
+            },
+            {
+                id: Math.random(), desc: "Concluir o curso",
+                estimateAt: new Date(), doneAt: null
+            },
+            {
+                id: Math.random(), desc: "Comprar curso de react-native",
+                estimateAt: new Date(), doneAt: new Date()
+            },
+            {
+                id: Math.random(), desc: "Concluir o curso",
+                estimateAt: new Date(), doneAt: null
+            },
+            {
+                id: Math.random(), desc: "Comprar curso de react-native",
+                estimateAt: new Date(), doneAt: new Date()
+            },
+            {
+                id: Math.random(), desc: "Concluir o curso",
+                estimateAt: new Date(), doneAt: null
+            },
+            {
+                id: Math.random(), desc: "Comprar curso de react-native",
+                estimateAt: new Date(), doneAt: new Date()
+            },
+            {
+                id: Math.random(), desc: "Concluir o curso",
+                estimateAt: new Date(), doneAt: null
+            },
+            {
+                id: Math.random(), desc: "Comprar curso de react-native",
+                estimateAt: new Date(), doneAt: new Date()
+            },
+            {
+                id: Math.random(), desc: "Concluir o curso",
+                estimateAt: new Date(), doneAt: null
+            },
+            {
+                id: Math.random(), desc: "Comprar curso de react-native",
+                estimateAt: new Date(), doneAt: new Date()
+            },
+            {
+                id: Math.random(), desc: "Concluir o curso",
+                estimateAt: new Date(), doneAt: null
+            },
+            {
+                id: Math.random(), desc: "Comprar curso de react-native",
+                estimateAt: new Date(), doneAt: new Date()
+            },
+            {
+                id: Math.random(), desc: "Concluir o curso",
+                estimateAt: new Date(), doneAt: null
+            },
+            {
+                id: Math.random(), desc: "Comprar curso de react-native",
+                estimateAt: new Date(), doneAt: new Date()
+            },
+            {
+                id: Math.random(), desc: "Concluir o curso",
+                estimateAt: new Date(), doneAt: null
+            },
         ],
-        visibleTasks:[],
-        showDoneTasks:true,
+        visibleTasks: [],
+        showDoneTasks: true,
     }
     filterTasks = () => {
         let visibleTasks = null
-        if (this.state.showDoneTasks){
+        if (this.state.showDoneTasks) {
             visibleTasks = [...this.state.tasks]
-        }else{
+        } else {
             const pending = task => task.doneAt === null
             visibleTasks = this.state.tasks.filter(pending)
         }
-        this.setState({visibleTasks})
+        this.setState({ visibleTasks })
     }
+    //o setState tem dois parametros, o segundo como uma função para pós execução do estado
     toogleFilter = () => {
-        this.setState({showDoneTasks:!this.state.showDoneTasks},this.filterTasks)
+        this.setState({ showDoneTasks: !this.state.showDoneTasks }, this.filterTasks)
     }
     toggleTask = id => {
-        const tasks = this.state.tasks.map(task=>{
-            if (task.id === id){
-                task = {...task}
+        const tasks = this.state.tasks.map(task => {
+            if (task.id === id) {
+                task = { ...task }
                 task.doneAt = task.doneAt ? null : new Date()
             }
             return task
         })
-        this.setState({tasks})
+        this.setState({ tasks }, this.filterTasks)
     }
-    render(){
+    componentDidMount = () => {
+        this.filterTasks()
+    }
+    render() {
         return (
 
             <View style={styles.container}>
                 <ImageBackground source={todayImage}
-                style={styles.background}>
-                <View style={styles.titleBar}>
-                    <Text style={styles.title}>Hoje</Text>
-                    <Text style={styles.subtitle}>
-                        {moment().locale('pt-br').format('ddd, D [de] MMMM')}
-                    </Text>
-                </View>
+                    style={styles.background}>
+                    <View style={styles.iconBar}>
+                        <TouchableOpacity onPress={this.toogleFilter}>
+                            <Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash'} size={20}
+                            color={commonStyle.colors.secondary}>
+
+                            
+                            </Icon>
+                        </TouchableOpacity>
+                    </View>
+
+                    <View style={styles.titleBar}>
+                        <Text style={styles.title}>Hoje</Text>
+                        <Text style={styles.subtitle}>
+                            {moment().locale('pt-br').format('ddd, D [de] MMMM')}
+                        </Text>
+                    </View>
                 </ImageBackground>
                 <View style={styles.tasksContainer}>
-                   <FlatList data={this.state.tasks} 
-                    keyExtractor={item=>'${item.id}'}
-                    renderItem={({item}) => <Task {...item} toggleTask={this.toggleTask} ></Task>}
-                   >
-                   
-                   </FlatList>
+                    <FlatList data={this.state.visibleTasks}
+                        keyExtractor={item => '${item.id}'}
+                        renderItem={({ item }) => <Task {...item} toggleTask={this.toggleTask} ></Task>}>
+
+                    </FlatList>
                 </View>
             </View>
         )
@@ -102,32 +148,38 @@ export default class Agenda extends Component{
 
 const styles = StyleSheet.create({
     container: {
-        flex:1,
-       
+        flex: 1,
+
     },
-    background:{
-        flex:3.
+    background: {
+        flex: 3.
     },
-    titleBar:{
-        flex:1,
+    titleBar: {
+        flex: 1,
         justifyContent: 'flex-end',
     },
-    title:{
+    title: {
         fontFamily: commonStyle.fontFamily,
         color: commonStyle.colors.secondary,
-        fontSize:50,
-        marginLeft:20,
-        marginBottom:10,
+        fontSize: 50,
+        marginLeft: 20,
+        marginBottom: 10,
     },
-    subtitle:{
+    subtitle: {
         fontFamily: commonStyle.fontFamily,
         color: commonStyle.colors.secondary,
-        fontSize:20,
-        marginLeft:20,
-        marginBottom:30,
+        fontSize: 20,
+        marginLeft: 20,
+        marginBottom: 30,
     },
-    tasksContainer:{
-        flex:7,
+    tasksContainer: {
+        flex: 7,
+    },
+    iconBar:{
+        marginTop:Platform.OS === 'ios' ? 30 : 10,
+        marginHorizontal: 20,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
     }
 
 })
